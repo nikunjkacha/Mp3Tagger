5f1fc55666245cd6aa58606e7b6b9c31aa4a1cad
Dennis <d.ciba@gmx.de>
2014-09-07 14:07:34 +0200

    Integrated UI-Elements for Tag Editing

--- Mp3Tagger/src/com/uncompilable/mp3tagger/TagEditFragment.java
+++ Mp3Tagger/src/com/uncompilable/mp3tagger/TagEditFragment.java
@@ -1,7 +1,23 @@
 package com.uncompilable.mp3tagger;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.NumberPicker;
 
 public class TagEditFragment extends Fragment {
 
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View root = inflater.inflate(R.layout.tagedit_fragment, container, false);
+		
+		NumberPicker npTrackNumber = (NumberPicker) root.findViewById(R.id.npTracknumber);
+		npTrackNumber.setMinValue(0);
+		npTrackNumber.setMaxValue(512);
+		
+		return root;
+	}
 }
