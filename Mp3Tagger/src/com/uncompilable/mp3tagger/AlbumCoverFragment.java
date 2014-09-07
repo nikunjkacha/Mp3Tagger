857e576ca4f5ce4414057401171e260990395c02
Dennis <d.ciba@gmx.de>
2014-09-07 15:30:06 +0200

    First version of the genre enum implemented

--- Mp3Tagger/src/com/uncompilable/mp3tagger/AlbumCoverFragment.java
+++ Mp3Tagger/src/com/uncompilable/mp3tagger/AlbumCoverFragment.java
@@ -1,7 +1,16 @@
 package com.uncompilable.mp3tagger;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class AlbumCoverFragment extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View root = inflater.inflate(R.layout.albumcover_fragment, container, false);
 
+		return root;
+	}
 }
